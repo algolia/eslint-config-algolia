87893508bf913e191050dba6c3af004c50c2d1fd
Matthieu Dumont <jerska@live.fr>
2015-12-07 12:06:51 +0100

    feat(eslint-plugin-algolia): Use v1.4.2
    Comes with:
    - force-import-root
    - no-require

--- rules/plugin.js
+++ rules/plugin.js
@@ -2,6 +2,10 @@ module.exports = {
   parser: 'babel-eslint',
   plugins: ['algolia'],
   rules: {
-    'algolia/relative-require': 2
+    'algolia/relative-require': 2,
+    // Don't force ES6 yet
+    // ES6 projects should override these
+    'algolia/force-import-root': 0, // Recommended 2
+    'algolia/no-require': 0 // Recommended 1
   }
 };
