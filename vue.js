9cbb9be34691f6ff1d00a3c1bb540e2bf3bb6f0a
vvo <vincent.voyer@algolia.com>
2017-06-16 09:45:47 +0200

    test eslint-plugin-vue

--- vue.js
+++ vue.js
@@ -1,5 +1,4 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
-  extends: ['./base.js', 'prettier'],
-  plugins: ['html'],
+  extends: ['./base.js', 'plugin:vue/recommended', 'prettier'],
 };
