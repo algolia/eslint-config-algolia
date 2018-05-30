a0d733a996a664fd3a4055866ad8fff0fa8b29a7
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-05-30 18:53:22 +0200

    feat(vue): use Vue plugin instead of the HTML one

--- vue.js
+++ vue.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
   // later we add vue-specific prettier extends here
-  extends: ['./base.js'],
-  plugins: ['html'],
+  extends: ['plugin:vue/strongly-recommended', './rules/vue.js', './base.js'],
+  plugins: ['vue'],
 };
