d0400a50dd967f0d4e5b97a22c0518f634559e2f
Yannick Croissant <yannick.croissant@gmail.com>
2019-04-15 11:18:15 +0200

    Disable @typescript-eslint/no-var-requires, conflict with import/no-commonjs

--- rules/typescript.js
+++ rules/typescript.js
@@ -47,7 +47,7 @@ module.exports = {
     ],
     '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
     '@typescript-eslint/no-useless-constructor': ['error'],
-    '@typescript-eslint/no-var-requires': ['error'],
+    '@typescript-eslint/no-var-requires': ['off'],
     '@typescript-eslint/prefer-for-of': ['off'],
     '@typescript-eslint/prefer-function-type': ['error'],
     '@typescript-eslint/prefer-interface': ['off'],
