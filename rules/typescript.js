3d0bdfe089dec47836533340a1dcc917976ed682
Yannick Croissant <yannick.croissant@gmail.com>
2019-04-16 10:45:01 +0200

    Enable @typescript-eslint/explicit-function-return-type rule

--- rules/typescript.js
+++ rules/typescript.js
@@ -8,7 +8,13 @@ module.exports = {
     '@typescript-eslint/ban-ts-ignore': ['off'],
     '@typescript-eslint/camelcase': ['error'],
     '@typescript-eslint/class-name-casing': ['error'],
-    '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+      },
+    ],
     '@typescript-eslint/explicit-member-accessibility': ['error'],
     '@typescript-eslint/generic-type-naming': ['off'],
     '@typescript-eslint/indent': ['off'],
