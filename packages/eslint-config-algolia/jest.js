09ff1c230a7d47cb0f97ba86035bb9d2a6c81332
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2021-08-27 11:55:09 +0200

    fix(jest): stricter configuration

--- packages/eslint-config-algolia/jest.js
+++ packages/eslint-config-algolia/jest.js
@@ -3,6 +3,17 @@ module.exports = {
   env: {
     jest: true,
   },
-  extends: ['plugin:jest/recommended'],
+  extends: ['plugin:jest/recommended', 'plugin:jest/style'],
   plugins: ['jest'],
+  rules: {
+    'consistent-test-it': ['error'],
+    'max-nested-describe': ['error', { max: 3 }],
+    'no-alias-methods': ['error'],
+    'no-duplicate-hooks': ['error'],
+    'no-test-return-statement': ['warn'],
+    'prefer-hooks-on-top': ['error'],
+    'prefer-spy-on': ['error'],
+    'require-to-throw-message': ['error'],
+    'require-top-level-describe': ['error'],
+  },
 };
