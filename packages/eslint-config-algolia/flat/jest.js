ef5612f36b6b0269e1847180bb9537e2d6d41d67
Sylvain Bellone <sylvain.bellone@algolia.com>
2024-10-15 11:53:42 +0200

    put algolia rules at the end

--- packages/eslint-config-algolia/flat/jest.js
+++ packages/eslint-config-algolia/flat/jest.js
@@ -4,11 +4,11 @@ const jestPlugin = require('eslint-plugin-jest');
 const jestRules = require('../rules/jest');
 
 module.exports = [
-  jestRules,
   {
     ...jestPlugin.configs['flat/recommended'],
   },
   {
     ...jestPlugin.configs['flat/style'],
   },
+  jestRules,
 ];
